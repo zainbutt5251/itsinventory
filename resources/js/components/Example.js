955fa025390ad07be2841cac62d4a3e88b86d91f
zain ul abiden <zainbutt5251>
2020-11-21 20:44:54 +0500

    'react-add'

---
+++ resources/js/components/Example.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// impor
+function Example() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Example Component</div>
+{/* <button className="btn btn-info">ffffff</button> */}
+                        <div className="card-body">I'm an example component!</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Example;
+
+if (document.getElementById('example')) {
+    ReactDOM.render(<Example />, document.getElementById('example'));
+}
